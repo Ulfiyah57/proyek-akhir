88a631009d5013fffef4f17b5ee948344442e791
_Pratista_Ulfiyah <155908436+Ulfiyah57@users.noreply.github.com>
2024-03-05 22:54:45 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,309 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import streamlit as st
+import urllib
+from babel.numbers import format_currency
+sns.set(style='dark')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+#Dataafterset
+class DataAnalyzer:
+    def __init__(self, df):
+        self.df = df
+#Daily Oders
+    def create_daily_orders_df(self):
+        daily_orders_df = self.df.resample(rule='D', on='order_approved_at').agg({
+            "order_id": "nunique",
+            "payment_value": "sum"
+        })
+        daily_orders_df = daily_orders_df.reset_index()
+        daily_orders_df.rename(columns={
+            "order_id": "order_count",
+            "payment_value": "revenue"
+        }, inplace=True)
+        
+        return daily_orders_df
+    
+    def review_score_df(self):
+        review_scores = self.df['review_score'].value_counts().sort_values(ascending=False)
+        most_common_score = review_scores.idxmax()
+
+        return review_scores, most_common_score
+#Review    
+    def create_order_status(self):
+        order_status_df = self.df["order_status"].value_counts().sort_values(ascending=False)
+        most_common_status = order_status_df.idxmax()
+
+        return order_status_df, most_common_status
+#Order Items
+    def create_sum_order_items_df(self):
+        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
+        sum_order_items_df.rename(columns={
+            "product_id": "product_count"
+        }, inplace=True)
+        sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
+
+        return sum_order_items_df
+#Spend Money   
+    def create_sum_spend_df(self):
+        sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
+            "payment_value": "sum"
+        })
+        sum_spend_df = sum_spend_df.reset_index()
+        sum_spend_df.rename(columns={
+            "payment_value": "total_spend"
+        }, inplace=True)
+
+        return sum_spend_df
+#State
+    def create_bystate_df(self):
+        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
+        bystate_df.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
+        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
+        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
+
+        return bystate_df, most_common_state 
+class BrazilMapPlotter:
+    def __init__(self,data, plt, mpimg, urllib, st):
+        self.data = data
+        self.plt = plt
+        self.mpimg = mpimg
+        self.urllib = urllib
+        self.st = st
+      
+    #def plot(self):
+    #    brazil = self.mpimg.imread(self.urllib.request.urlopen('https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'),'jpg')
+    #
+    #    # Check if the columns exist in the DataFrame
+    #    if "geolocation_lng" in self.data.columns and "geolocation_lat" in self.data.columns:
+    #        ax = self.data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", figsize=(10,10), alpha=0.3, s=0.3, c='maroon')
+    #        self.plt.axis('off')
+    #        self.plt.imshow(brazil, extent=[-73.98283055, -33.8, -33.75116944, 5.4])
+    #        self.st.pyplot()
+    #    else:
+    #        print("Columns 'geolocation_lng' and 'geolocation_lat' not found in the DataFrame.")
+    
+    def plot(self):
+        brazil = self.mpimg.imread(self.urllib.request.urlopen('https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'), 'jpg')
+    
+        fig, ax = self.plt.subplots(figsize=(10, 10))
+        ax.imshow(brazil, extent=[-73.98283055, -33.8, -33.75116944, 5.4])
+    
+        # Specify image coordinates for the point on the image
+        image_coords = {'x': 0, 'y': 0}
+    
+        # Plot the point on the image
+        ax.scatter(image_coords['x'], image_coords['y'], s=30, c='maroon', alpha=0.5)
+    
+        self.plt.axis('off')
+        self.st.pyplot()
+
+
+        
+# Dataset
+datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all_df = pd.read_csv("all_data.csv", encoding='latin-1')
+all_df.sort_values(by="order_approved_at", inplace=True)
+all_df.reset_index(inplace=True)
+
+# Convert "order_approved_at" to datetime
+all_df["order_approved_at"] = pd.to_datetime(all_df["order_approved_at"], errors='coerce')
+
+# Filter data
+min_date = all_df["order_approved_at"].min().date()  # Convert datetime64 to date
+max_date = all_df["order_approved_at"].max().date()  # Convert datetime64 to date
+
+# Sidebar
+with st.sidebar:
+    # Title
+    st.title("Ulfiyah Nahdi Pratista")
+
+    # Logo Image
+    st.image("streamlit-mark-color.png")
+
+    # Date Range
+    start_date, end_date = st.date_input(
+        label="Select Date Range",
+        value=[min_date, max_date],
+        min_value=min_date,
+        max_value=max_date
+    )
+
+# Main
+# Main
+# Convert start_date and end_date to datetime objects
+start_date = pd.to_datetime(start_date)
+end_date = pd.to_datetime(end_date)
+
+main_df = all_df[(all_df["order_approved_at"] >= start_date) & (all_df["order_approved_at"] <= end_date)]
+
+map_plot = BrazilMapPlotter(all_df, plt, mpimg, urllib, st)
+
+daily_orders_df = DataAnalyzer(all_df).create_daily_orders_df()
+sum_spend_df = DataAnalyzer(all_df).create_sum_spend_df()
+sum_order_items_df = DataAnalyzer(all_df).create_sum_order_items_df()
+review_score, common_score = DataAnalyzer(all_df).review_score_df()
+state, most_common_state = DataAnalyzer(all_df).create_bystate_df()
+order_status, common_status = DataAnalyzer(all_df).create_order_status()
+
+# Title
+st.header("E-Commerce Dashboard :convenience_store:")
+
+# Daily Orders
+st.subheader("Daily Orders")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_order = daily_orders_df["order_count"].sum()
+    st.markdown(f"Total Order: **{total_order}**")
+
+with col2:
+    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
+    st.markdown(f"Total Revenue: **{total_revenue}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(
+    daily_orders_df["order_approved_at"],
+    daily_orders_df["order_count"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# Review Score
+st.subheader("Review Score")
+col1,col2 = st.columns(2)
+
+with col1:
+    avg_review_score = review_score.mean()
+    st.markdown(f"Average Review Score: **{avg_review_score}**")
+
+with col2:
+    most_common_review_score = review_score.value_counts().index[0]
+    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.barplot(x=review_score.index, 
+            y=review_score.values, 
+            order=review_score.index,
+            palette=["#068DA9" if score == common_score else "#D3D3D3" for score in review_score.index]
+            )
+
+plt.title("Rating by customers for service", fontsize=15)
+plt.xlabel("Rating")
+plt.ylabel("Count")
+plt.xticks(fontsize=12)
+st.pyplot(fig)
+
+# Order Items
+st.subheader("Order Items")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["product_count"].sum()
+    st.markdown(f"Total Items: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["product_count"].mean()
+    st.markdown(f"Average Items: **{avg_items}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+
+colors = ["#068DA9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Produk paling banyak terjual", loc="center", fontsize=50)
+ax[0].tick_params(axis ='y', labelsize=35)
+ax[0].tick_params(axis ='x', labelsize=30)
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+# Spend Money
+st.subheader("Customer Spend Money")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_spend = format_currency(sum_spend_df["total_spend"].sum(), "IDR", locale="id_ID")
+    st.markdown(f"Total Spend: **{total_spend}**")
+
+with col2:
+    avg_spend = format_currency(sum_spend_df["total_spend"].mean(), "IDR", locale="id_ID")
+    st.markdown(f"Average Spend: **{avg_spend}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(
+    sum_spend_df["order_approved_at"],
+    sum_spend_df["total_spend"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# State
+st.subheader("Customer Demographic")
+tab1, tab2, tab3 = st.tabs(["State", "Order Status", "Geolocation"])
+
+with tab1:
+    most_common_state = state.customer_state.value_counts().index[0]
+    st.markdown(f"Most Common State: **{most_common_state}**")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=state.customer_state.value_counts().index,
+                y=state.customer_count.values, 
+                data=state,
+                palette=["#068DA9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
+                    )
+    plt.title("Number customers from State", fontsize=15)
+    plt.xlabel("State")
+    plt.ylabel("Number of Customers")
+    plt.xticks(fontsize=12)
+    st.pyplot(fig)
+
+with tab2:
+    common_status_ = order_status.value_counts().index[0]
+    st.markdown(f"Most Common Order Status: **{common_status_}**")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=order_status.index,
+                y=order_status.values,
+                order=order_status.index,
+                palette=["#068DA9" if score == common_status else "#D3D3D3" for score in order_status.index]
+                )
+    
+    plt.title("Order Status", fontsize=15)
+    plt.xlabel("Status")
+    plt.ylabel("Count")
+    plt.xticks(fontsize=12)
+    st.pyplot(fig)
+
+with tab3:
+    map_plot.plot()
+
+    with st.expander("See Explanation"):
+        st.write('Sesuai dengan grafik yang sudah dibuat, ada lebih banyak pelanggan di bagian tenggara dan selatan. Informasi lainnya, ada lebih banyak pelanggan di kota-kota yang merupakan ibu kota (São Paulo, Rio de Janeiro, Porto Alegre, dan lainnya).')
+
+st.caption('Copyright (C) Ulfiyah Nahdi Pratista 2024')
+
